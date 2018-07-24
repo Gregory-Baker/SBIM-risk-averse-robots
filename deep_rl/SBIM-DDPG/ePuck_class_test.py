8cb2745e040591d236f7c422b11d9a223459c439
Greg Baker <gregory.baker@brl.ac.uk>
2018-07-24 17:51:12 +0100

    read prox sens working

--- deep_rl/SBIM-DDPG/ePuck_class_test.py
+++ deep_rl/SBIM-DDPG/ePuck_class_test.py
@@ -24,6 +24,9 @@ class ePuck:
     wheel_sep = 0.053   # distance between wheels (m)
     wheel_rad = 0.02    # radius of wheels (m)
     
+    # Braitenberg weights for obstacle avoidance
+    brait_weights_leftMotor = [-1,1,2,-2,-1,0,0,0]
+    noDetectionDistance = 0.04
     
 #----------------------------------------------------------------------------
         
@@ -32,62 +35,56 @@ class ePuck:
         self.name = 'ePuck#' + self.i
         
         # Get object handles
-        returnCode, self.handle = vrep.simxGetObjectHandle(
-                clientID, self.name, vrep.simx_opmode_blocking)
+        returnCode, self.handle = vrep.simxGetObjectHandle(clientID, self.name, vrep.simx_opmode_blocking)
         
-        returnCode, self.leftJoint = vrep.simxGetObjectHandle(
-                clientID, 'ePuck_leftJoint#' + self.i, vrep.simx_opmode_blocking)
-        returnCode, self.rightJoint = vrep.simxGetObjectHandle(
-                clientID, 'ePuck_rightJoint#' + self.i, vrep.simx_opmode_blocking)
+        returnCode, self.leftJoint = vrep.simxGetObjectHandle(clientID, 'ePuck_leftJoint#' + self.i, vrep.simx_opmode_blocking)
+                                                              
+        returnCode, self.rightJoint = vrep.simxGetObjectHandle(clientID, 'ePuck_rightJoint#' + self.i, vrep.simx_opmode_blocking)
         
-        returnCode, self.link = vrep.simxGetObjectHandle(
-                clientID, 'ePuck_link' + self.i, vrep.simx_opmode_blocking)
+        returnCode, self.link = vrep.simxGetObjectHandle(clientID, 'ePuck_link' + self.i, vrep.simx_opmode_blocking)
         
         self.proxSensorHandle = [None]*8
         for j in range(0,8):
-            returnCode, self.proxSensorHandle[j] = vrep.simxGetObjectHandle(
-                    clientID, 'ePuck_proxSensor' + str(j+1) + '#' + self.i, vrep.simx_opmode_blocking)
+            returnCode, self.proxSensorHandle[j] = vrep.simxGetObjectHandle(clientID, 'ePuck_proxSensor' + str(j+1) + '#' + self.i, vrep.simx_opmode_blocking)
     
         # Setup object streaming calls       
-        returnCode, linearVelocity, angularVelocity = vrep.simxGetObjectVelocity(
-                clientID, self.handle, vrep.simx_opmode_streaming)
+        returnCode, linearVelocity, angularVelocity = vrep.simxGetObjectVelocity(clientID, self.handle, vrep.simx_opmode_streaming)
         
-        returnCode, state, forceVectorLink, torqueVector = vrep.simxReadForceSensor(
-                clientID, self.link, vrep.simx_opmode_streaming)
+        returnCode, state, forceVectorLink, torqueVector = vrep.simxReadForceSensor(clientID, self.link, vrep.simx_opmode_streaming)
         
-        returnCode, eulerAngles = vrep.simxGetObjectOrientation(
-                clientID, self.handle, -1, vrep.simx_opmode_streaming)
+        returnCode, eulerAngles = vrep.simxGetObjectOrientation(clientID, self.handle, -1, vrep.simx_opmode_streaming)
         
-        returnCode, position = vrep.simxGetObjectPosition(
-                clientID, self.handle, -1, vrep.simx_opmode_streaming)
+        returnCode, position = vrep.simxGetObjectPosition(clientID, self.handle, -1, vrep.simx_opmode_streaming)
         
         self.proxSensor = [None]*8
+        self.proxPoint = [None]*8
         self.proxDist = [None]*8
         for i in range(0,8):
-           returnCode, self.proxSensor[i], self.proxDist[i], _, _ = vrep.simxReadProximitySensor(
-                   clientID, self.proxSensorHandle[i], vrep.simx_opmode_streaming) 
+           returnCode, self.proxSensor[i], self.proxPoint[i], _, _ = vrep.simxReadProximitySensor(clientID, self.proxSensorHandle[i], vrep.simx_opmode_streaming) 
         
 #----------------------------------------------------------------------------
 # Setters
         
-    def set_pos(self, position: '[x y] metres'):
+    def set_pos(self, 
+                position: '[x y] metres'):
         
         if len(position)==2:
             position.append(0.01915)
         
-        vrep.simxSetObjectPosition(
-                clientID, self.handle, -1, position, vrep.simx_opmode_oneshot)
+        vrep.simxSetObjectPosition(clientID, self.handle, -1, position, vrep.simx_opmode_oneshot)
 
-    def set_ang(self, angle: 'rad'):
+    def set_ang(self, 
+                angle: 'rad'):
         
         # Convert angle to euler angle
         eulerAngles = [0, 0, angle]
         
         # Set orientation
-        vrep.simxSetObjectOrientation(
-                clientID, self.handle, -1, eulerAngles, vrep.simx_opmode_oneshot)
+        vrep.simxSetObjectOrientation(clientID, self.handle, -1, eulerAngles, vrep.simx_opmode_oneshot)
     
-    def set_vel(self, linear_velocity: 'm/s', angular_velocity: 'rad/sec'):
+    def set_vel(self, 
+                linear_velocity: 'm/s', 
+                angular_velocity: 'rad/sec'):
         
         # Convert to wheel velocities
         vel_r = linear_velocity + self.wheel_sep*angular_velocity/2
@@ -98,10 +95,8 @@ class ePuck:
         omega_l = vel_l/self.wheel_rad
         
         # Set wheel velocities
-        vrep.simxSetJointTargetVelocity(
-                clientID, self.leftJoint, omega_l, vrep.simx_opmode_streaming)
-        vrep.simxSetJointTargetVelocity(
-                clientID, self.rightJoint, omega_r, vrep.simx_opmode_streaming)
+        vrep.simxSetJointTargetVelocity(clientID, self.leftJoint, omega_l, vrep.simx_opmode_streaming)
+        vrep.simxSetJointTargetVelocity(clientID, self.rightJoint, omega_r, vrep.simx_opmode_streaming)
 
 #----------------------------------------------------------------------------
 # Getters
@@ -124,8 +119,7 @@ class ePuck:
         
     def get_vel(self):
         
-        returnCode, linearVelocity, angularVelocity = vrep.simxGetObjectVelocity(
-                clientID, self.handle, vrep.simx_opmode_buffer)        
+        returnCode, linearVelocity, angularVelocity =                   vrep.simxGetObjectVelocity(clientID, self.handle, vrep.simx_opmode_buffer)
         
 #----------------------------------------------------------------------------
 # Other functions
@@ -133,13 +127,13 @@ class ePuck:
     def read_prox_sens(self):
         
         for i in range(0,8):
-           returnCode, self.proxSensor[i], self.proxDist[i], _, _ = vrep.simxReadProximitySensor(
-                   clientID, self.proxSensorHandle[i], vrep.simx_opmode_buffer) 
-               
+            returnCode, self.proxSensor[i], self.proxPoint[i], _, _ = vrep.simxReadProximitySensor(clientID, self.proxSensorHandle[i], vrep.simx_opmode_buffer)
         
-    def move_to_target(self,
-                       target: '[x y] metres',
-                       linear_velocity: 'm/s',
+            self.proxDist[i] = self.proxSensor[i]*np.linalg.norm(np.array(self.proxPoint[i]))
+        
+    def move_to_target(self, 
+                       target: '[x y] metres', 
+                       linear_velocity: 'm/s', 
                        proximity_to_target = 0.05):
 
         self.get_pos()
@@ -155,7 +149,14 @@ class ePuck:
             self.get_ang()
             self.read_prox_sens()
             
-            
+            # Check if front and side prox sens are activated
+            if(sum(self.proxDist[0:6])>0):
+                velRight = linear_velocity
+                velLeft = linear_velocity
+                if(sum(self.proxDist[1:5])>0):
+                    for i in range(1,5):
+                        velLeft = velLeft + linear_velocity*brait_weights_leftMotor[i]*(1-(self.proxDist[i]/noDetectionDistance))
+                                
                       
             position_np = np.array(self.position) 
             
